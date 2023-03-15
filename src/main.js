cc862b3e4df6167e706d1774c4f82e2945d3cc14
Coen <458416@student.fontys.nl>
2023-03-15 15:28:22 +0100

    Update from Bootstrap-vue Chaos

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 // import './assets/main.css'
 
+// Make BootstrapVue available throughout your project
+// Vue.use(BootstrapVue)
 
 const app = createApp(App)
 
